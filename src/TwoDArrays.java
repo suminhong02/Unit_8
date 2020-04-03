33c9b0c2a836e21c2ab7235fd97a35e21294f2a5
suminhong02 <hongsu@ssfs.org>
2020-04-03 18:07:47 +0900

    Completed TwoDArrays

---
+++ src/TwoDArrays.java
@@ -0,0 +1,24 @@
+public class TwoDArrays {
+
+    public static int maxValue(int[][] list){
+        int max = list[0][0];
+        for(int row = 0; row<list.length; row++){
+            for(int col = 0; col<list[0].length; col++){
+                if(list[row][col]>max){
+                    max = list[row][col];
+                }
+            }
+        }
+        return max;
+    }
+
+    public static int sumAll(int[][] arr){
+        int sum = 0;
+        for(int row = 0; row<arr.length; row++){
+            for(int col = 0; col<arr[0].length; col++){
+                sum += arr[row][col];
+            }
+        }
+        return sum;
+    }
+}
