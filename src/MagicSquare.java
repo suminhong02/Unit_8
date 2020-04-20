8d9358590537ff42379022a10daa7678d48fa5d2
suminhong02 <hongsu@ssfs.org>
2020-04-20 18:37:59 +0900

    Completed MagicSquare

--- src/MagicSquare.java
+++ src/MagicSquare.java
@@ -1,16 +1,26 @@
 /**
- * This class determines if a square grid of any size is a magic square which has the same number of rows and columns and the sum of each rows, columns, and diagonal lines should be equal.
+ * This class determines if a square grid of any size is a magic square which has the same number of rows and columns and the sum of each rows,
+ * columns, and diagonal lines should be equal.
  * @author Sumin Hong
  * @since 4/3/2020
  */
+
 public class MagicSquare {
     private int[][] grid;
 
+    /**
+     * Constructor with given array numbers by user.
+     * @param g arrangement of numbers in the grid.
+     */
     public MagicSquare(int[][] g) {
         grid = g;
     }
 
-
+    /**
+     * This method gives the sum of each row.
+     * @param row the row of grid
+     * @return sum of row
+     */
     public int rowSum(int row) {
         // <<< Complete the code >>>
         int sum = 0;
@@ -21,7 +31,11 @@ public class MagicSquare {
             return sum;
     }
 
-
+    /**
+     * This method gives the sum of each column.
+     * @param col the column of grid
+     * @return sum of column
+     */
     public int colSum(int col) {
         // <<< Complete the code >>>
         int sum = 0;
@@ -32,7 +46,10 @@ public class MagicSquare {
         return sum;
     }
 
-
+    /**
+     * This method give the sum of numbers in up-diagonal.
+     * @return sum of up-diagonal numbers.
+     */
     public int upDiagSum() {
         // <<< Complete the code >>>
         int sum = 0;
@@ -46,7 +63,10 @@ public class MagicSquare {
         return sum;
     }
 
-
+    /**
+     * This method gives the sum of numbers in down-diagonal.
+     * @return sum og down-diagonal numbers.
+     */
     public int downDiagSum() {
         // <<< Complete the code >>>
         int sum = 0;
@@ -56,11 +76,14 @@ public class MagicSquare {
         return sum;
     }
 
-
+    /**
+     * This method checks if the given array is a magic square, which means all sums of each row, column, diagonals are equal.
+     * @return true or false
+     */
     public boolean isMagicSquare() {
         // <<< Complete the code >>>
-        int row = grid.length;
-        int col = grid[row].length;
+        int row = grid.length-1;
+        int col = grid[row].length-1;
         if(rowSum(row) == colSum(col) && colSum(col) == upDiagSum() && upDiagSum() == downDiagSum()){
             return true;
         }
